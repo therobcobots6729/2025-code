e91d2fcd36a81fadc6b39aaab50a2ce5e6fd5f61
ATurner8133 <156738528+ATurner8133@users.noreply.github.com>
2025-01-29 23:16:20 -0500

    Update Robot.java
    added a test in robot to see if pid controls can start when the robot is turned on

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.flippy;
 
 
 /**
@@ -30,6 +31,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    flippy.leftPivot.setVoltage(flippy.wristFeedForward.calculate(flippy.wristAngle, 0));
+    flippy.rightPivot.setVoltage(flippy.wristFeedForward.calculate(flippy.wristAngle, 0));
     // Instantiate our RobotContainer. This will perform all our button bindings,
     // and put our
     // autonomous chooser on the dashboard.
@@ -58,6 +61,8 @@ public class Robot extends TimedRobot {
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
   public void disabledInit() {
+    flippy.leftPivot.setVoltage(flippy.wristFeedForward.calculate(flippy.wristAngle, 0));
+    flippy.rightPivot.setVoltage(flippy.wristFeedForward.calculate(flippy.wristAngle, 0));
     /*for (var i = 0; i < StatusLED.ledsBuffer.getLength(); i++) {
       StatusLED.ledsBuffer.setRGB(i, 0, 0, 255);
     }
